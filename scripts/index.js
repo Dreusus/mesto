8ad53f10abf91eb59177dc37098801c236685372
Andrey <dreususJ@yandex.ru>
2022-03-26 16:06:09 +0300

    ggwp

--- scripts/index.js
+++ scripts/index.js
@@ -3,39 +3,54 @@ const profileTitle= document.querySelector('.profile__title');
 const profileSubtitle= document.querySelector('.profile__subtitle');
 const closePopup = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
+const like=document.querySelector('.element__like')
+
+
+like.addEventListener('click', likeCard)
+
+function likeCard() {
+  like.classList.toggle('element__like_active')
+}
+
+
 
 
 
 const inputForm = document.querySelector('.popup__form');
 
-function openPopup() {
- popup.classList.add('popup_opened'); 
+function openProfilePopup() {
+ popup.classList.add('popup_opened');
+ inputName.value = profileTitle.textContent;
+ inputAbout.value = profileSubtitle.textContent;
 }
+
+editButton.addEventListener('click', openProfilePopup)
+
+
+
+
 
 function exitPopup() {
 popup.classList.remove('popup_opened')
 }
 
-editButton.addEventListener('click', openPopup)
+
 
 closePopup.addEventListener('click', exitPopup)
 
 const inputName = document.querySelector('#profile__name')
 const inputAbout = document.querySelector('#profile__about')
 
-function PopupInput() {
-inputName.value = profileTitle.textContent;
-inputAbout.value = profileSubtitle.textContent;
-}
-
-editButton.addEventListener('click', PopupInput);
 
 
-function formSubmitHandler (evt) {
+
+
+
+function handleProfileFormSubmit (evt) {
   evt.preventDefault();
   profileTitle.textContent = inputName.value;
   profileSubtitle.textContent = inputAbout.value;
   exitPopup();
 }
 
-  inputForm.addEventListener('submit', formSubmitHandler);
+  inputForm.addEventListener('submit', handleProfileFormSubmit);
